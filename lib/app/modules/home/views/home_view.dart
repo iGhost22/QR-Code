81f8d6cd403c7beb01637fc817e2b1d70a276e55
Rhymings <huynh.lam.dat.77@gmail.com>
2024-08-02 14:08:54 +0700

    Update HomeView

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -18,11 +18,71 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: GridView.builder(
+        itemCount: 4,
+        padding: const EdgeInsets.all(20),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
         ),
+        itemBuilder: (context, index) {
+          late String title;
+          late IconData icon;
+          late VoidCallback onTap;
+
+          switch (index) {
+            case 0:
+              title = "Add Product";
+              icon = Icons.post_add_rounded;
+              onTap = () {
+                Get.toNamed(Routes.addProduct);
+              };
+              break;
+            case 1:
+              title = "Products";
+              icon = Icons.list_alt_outlined;
+              onTap = () {
+                Get.toNamed(Routes.products);
+              };
+              break;
+            case 2:
+              title = "QR Code";
+              icon = Icons.qr_code;
+              onTap = () {
+                print("OPEN CAMERA");
+              };
+              break;
+            case 3:
+              title = "Catalog";
+              icon = Icons.document_scanner_outlined;
+              onTap = () {
+                print("OPEN PDF");
+              };
+              break;
+          }
+
+          return Material(
+            color: Colors.grey.shade300,
+            borderRadius: BorderRadius.circular(10),
+            child: InkWell(
+              onTap: onTap,
+              borderRadius: BorderRadius.circular(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Icon(icon, size: 50),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(title),
+                ],
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -33,7 +93,7 @@ class HomeView extends GetView<HomeController> {
             Get.snackbar("Error", hasil['error']);
           }
         },
-        child: Icon(Icons.logout),
+        child: const Icon(Icons.logout),
       ),
     );
   }
