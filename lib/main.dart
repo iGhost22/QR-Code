a05575816c9ff16dae95b03483e320de9f18cbee
Rhymings <huynh.lam.dat.77@gmail.com>
2024-08-02 08:50:08 +0700

    Connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:qr_code/app/modules/loading/loading_screen.dart';
+import 'package:qr_code/firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      title: "QR Code",
-      initialRoute: Routes.login,
-      getPages: AppPages.routes,
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+        stream: null,
+        builder: (context, snapAuth) {
+          if (snapAuth.connectionState == ConnectionState.waiting) {
+            return const LoadingScreen();
+          }
+          return GetMaterialApp(
+            title: "QR Code",
+            initialRoute: snapAuth.hasData ? Routes.home : Routes.login,
+            getPages: AppPages.routes,
+          );
+        });
+  }
 }
